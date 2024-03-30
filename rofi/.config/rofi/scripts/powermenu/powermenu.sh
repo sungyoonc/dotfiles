47712b660744ffd69f3a04255e009e968d7171ab
Sungyoon Cho <sungyoon@sungyooncho.dev>
2024-03-30 17:27:49 +0900

    fix(rofi): broken icon

--- rofi/.config/rofi/scripts/powermenu/powermenu.sh
+++ rofi/.config/rofi/scripts/powermenu/powermenu.sh
@@ -15,110 +15,111 @@ theme='style-1'
 
 # CMDs
 uptime="$(uptime -p | sed -e 's/up //g')"
-host=$(hostname)
+host=$(cat /proc/sys/kernel/hostname)
 
 # Options
-shutdown=' Shutdown'
-reboot=' Reboot'
-lock=' Lock'
-suspend=' Suspend'
-logout=' Logout'
+shutdown=' Shutdown'
+reboot=' Reboot'
+lock=' Lock'
+suspend='󰤄 Suspend'
+logout=' Logout'
 hibernate=' Hibernate'
-yes=' Yes'
-no=' No'
+yes=' Yes'
+no=' No'
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "$host" \
-		-mesg "Uptime: $uptime" \
-		-theme ${dir}/${theme}.rasi
+    rofi -dmenu \
+        -p "$host" \
+        -mesg "Uptime: $uptime" \
+        -theme ${dir}/${theme}.rasi
 }
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
+    rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
+        -theme-str 'mainbox {children: [ "message", "listview" ];}' \
+        -theme-str 'listview {columns: 2; lines: 1;}' \
+        -theme-str 'element-text {horizontal-align: 0.5;}' \
+        -theme-str 'textbox {horizontal-align: 0.5;}' \
+        -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Are you Sure?' \
+        -theme ${dir}/${theme}.rasi
 }
 
 # Ask for confirmation
 confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
+    echo -e "$yes\n$no" | confirm_cmd
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$hibernate\n$logout\n$reboot\n$shutdown" | rofi_cmd
+    echo -e "$lock\n$suspend\n$hibernate\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--suspend' ]]; then
-			# mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--hibernate' ]]; then
-			systemctl hibernate
-		elif [[ $1 == '--logout' ]]; then
-			if [[ "$XDG_CURRENT_DESKTOP" == 'Hyprland' ]]; then
-				# close all client windows
-				# required for graceful exit since many apps aren't good SIGNAL citizens
-				HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
-				hyprctl --batch "$HYPRCMDS" >>/tmp/hypr/hyprexitwithgrace.log 2>&1
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--suspend' ]]; then
+            # mpc -q pause
+            amixer set Master mute
+            systemctl suspend
+        elif [[ $1 == '--hibernate' ]]; then
+            systemctl hibernate
+        elif [[ $1 == '--logout' ]]; then
+            if [[ "$XDG_CURRENT_DESKTOP" == 'Hyprland' ]]; then
+                # close all client windows
+                # required for graceful exit since many apps aren't good SIGNAL citizens
+                HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
+                hyprctl --batch "$HYPRCMDS" >>/tmp/hypr/hyprexitwithgrace.log 2>&1
 
-				# exit hyprland
-				hyprctl dispatch exit >>/tmp/hypr/hyprexitwithgrace.log 2>&1
-			elif [[ "$XDG_CURRENT_DESKTOP" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$XDG_CURRENT_DESKTOP" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$XDG_CURRENT_DESKTOP" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$XDG_CURRENT_DESKTOP" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			fi
-		fi
-	else
-		exit 0
-	fi
+                # exit hyprland
+                # loginctl kill-user $(whoami) >>/tmp/hypr/hyprexitwithgrace.log 2>&1
+                hyprctl dispatch exit
+            elif [[ "$XDG_CURRENT_DESKTOP" == 'openbox' ]]; then
+                openbox --exit
+            elif [[ "$XDG_CURRENT_DESKTOP" == 'bspwm' ]]; then
+                bspc quit
+            elif [[ "$XDG_CURRENT_DESKTOP" == 'i3' ]]; then
+                i3-msg exit
+            elif [[ "$XDG_CURRENT_DESKTOP" == 'plasma' ]]; then
+                qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+            fi
+        fi
+    else
+        exit 0
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
 $shutdown)
-	run_cmd --shutdown
-	;;
+    run_cmd --shutdown
+    ;;
 $reboot)
-	run_cmd --reboot
-	;;
+    run_cmd --reboot
+    ;;
 $lock)
-	if [[ -x '/usr/bin/betterlockscreen' ]]; then
-		betterlockscreen -l
-	elif [[ -x '/usr/bin/swaylock' ]]; then
-    swaylock
-	fi
-	;;
+    if [[ -x '/usr/bin/betterlockscreen' ]]; then
+        betterlockscreen -l
+    elif [[ -x '/usr/bin/swaylock' ]]; then
+        swaylock
+    fi
+    ;;
 $suspend)
-	run_cmd --suspend
-	;;
+    run_cmd --suspend
+    ;;
 $hibernate)
-	run_cmd --hibernate
-	;;
+    run_cmd --hibernate
+    ;;
 $logout)
-	run_cmd --logout
-	;;
+    run_cmd --logout
+    ;;
 esac
